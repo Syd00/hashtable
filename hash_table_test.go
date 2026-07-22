45029071a388b2f1cde9f2e26bf74c6c08f04a66
Syd00 <77528914+Syd00@users.noreply.github.com>
2026-07-22 16:09:10 +0200

    Add benchmark

--- hash_table_test.go
+++ hash_table_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestConcorre(t *testing.T) {
+func TestConcurrency(t *testing.T) {
 	ht := NewHashTable[int, int](10, func(k int) int { return k })
 	var wg sync.WaitGroup
 
@@ -28,5 +28,29 @@ func TestConcorre(t *testing.T) {
 
 	if ht.count != 500 {
 		t.Errorf("Expected 500, got %d", ht.count)
+	}
+}
+
+// Benchmark della nostra Custom Hash Table
+func BenchmarkCustomHashTable(b *testing.B) {
+	ht := NewHashTable[string, int](1024, StringHash)
+
+	b.ResetTimer() // Azzera il tempo di creazione della tabella
+	for i := 0; i < b.N; i++ {
+		key := "key_" + string(rune(i%100))
+		ht.Put(key, i)
+		_, _ = ht.Get(key)
+	}
+}
+
+// Benchmark della 'map' nativa di Go (per confronto!)
+func BenchmarkNativeMap(b *testing.B) {
+	m := make(map[string]int, 1024)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key := "key_" + string(rune(i%100))
+		m[key] = i
+		_ = m[key]
 	}
 }
